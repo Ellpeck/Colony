882bd8af9d5a18148bbe06f7c8bff828880b78e3
Ellpeck <megamaximal@gmail.com>
2019-06-08 12:38:31 +0200

    start of people and selections

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour {
+
+    public GameObject selectedObject;
+    public LayerMask objectLayers;
+
+    private new Camera camera;
+
+    private void Start() {
+        this.camera = Camera.main;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            var pos = Input.mousePosition;
+            var ray = this.camera.ScreenPointToRay(pos);
+            var trace = Physics2D.GetRayIntersection(ray, Mathf.Infinity, this.objectLayers);
+            this.selectedObject = trace ? trace.transform.gameObject : null;
+        }
+    }
+
+}
