c1cd0b6d8f1968efe541234f6e29ddee7659281e
Ellpeck <megamaximal@gmail.com>
2019-06-11 18:28:33 +0200

    speed up button

---
+++ Assets/Scripts/SpeedUpButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpButton : MonoBehaviour {
+
+    public float speedUpFactor;
+
+    public void OnSelect() {
+        Time.timeScale *= this.speedUpFactor;
+    }
+
+    public void OnDeselect() {
+        Time.timeScale /= this.speedUpFactor;
+    }
+
+}
